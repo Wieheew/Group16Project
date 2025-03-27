9196a062357066bcf4471fe2caf4217a888e51f9
Ethan Wiehe <wieheew@mail.uc.edu>
2025-03-27 11:52:12 -0400

    Add project files.

---
+++ Group16Project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group16Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reds myReds = new Reds(); //Declare and instantiate
+            myReds.Price = 200; //Invoke the setter
+            Console.WriteLine("The price of my ticket is " + myReds.Price);
+            myReds.Section = 112;
+            Console.WriteLine("The section I am siting in is " + myReds.Section);
+
+        }
+    }
+}
---
+++ Group16Project/Reds.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group16Project
+{
+    /// <summary>
+    /// Model the price of reds tickets based on section
+    /// </summary>
+    class Reds
+    {
+        private double _price;
+        private double _section;
+
+        public double Price { get { return _price; } set { _price = value; } } //getter and setter for price
+        public double Section { get { return _section; } set { _section = value; } } // getter and setter for section
+
+    }
+}
